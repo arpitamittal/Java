f4f046df74fb4edd8ba480ae85e6e1ca4d10697c
Arpita <arpitamittal66@gmail.com>
2021-04-23 13:00:55 +0530

    First commit

---
+++ Java/src/main/java/com/arpitamittal/Java/App.java
@@ -0,0 +1,13 @@
+package com.arpitamittal.Java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
