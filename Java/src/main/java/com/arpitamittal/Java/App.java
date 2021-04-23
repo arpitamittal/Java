4519d644e2de2c283a5c22de80f8362f391d17dc
Arpita <62317642+arpitamittal@users.noreply.github.com>
2021-04-23 13:13:05 +0530

    Changes done

--- Java/src/main/java/com/arpitamittal/Java/App.java
+++ Java/src/main/java/com/arpitamittal/Java/App.java
@@ -7,7 +7,8 @@ package com.arpitamittal.Java;
 public class App 
 {
     public static void main( String[] args )
-    {
+    { 
+        System.out.pritln("My happy place");
         System.out.println( "Hello World!" + "Let's learn to code in Java" );
     }
 }
