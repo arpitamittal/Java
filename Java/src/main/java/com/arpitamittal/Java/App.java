555cc93441089f641488b5038d471e6f88634d52
Arpita <arpitamittal66@gmail.com>
2021-04-23 13:15:49 +0530

    second commit for changes

--- Java/src/main/java/com/arpitamittal/Java/App.java
+++ Java/src/main/java/com/arpitamittal/Java/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     { 
-        System.out.pritln("My happy place");
+        System.out.println("My happy place");
         System.out.println( "Hello World!" + "Let's learn to code in Java" );
     }
 }
